24209ba6fc5f2a5337b6da161507c077d4636ee3
MariaZanahoria <m.iselacarrillo.p@gmail.com>
2019-10-07 19:39:07 -0500

    menu de navegacion de url bak-end

--- src/Components/Waitress/MenuNav.js
+++ src/Components/Waitress/MenuNav.js
@@ -1,150 +1,50 @@
 import React, { Component } from "react";
 import "./Menu.css";
-import menuChurros from "../Menu";
-import Comanda from "./Comanda";
 import Axios from 'axios';
 
-class Menu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      subCategory: [],
-      selectedItem: [],
-      total: 0
-    };
-  }
-  componentDidMount() {
-    Axios.get('https://churro-latte.herokuapp.com/products')
-    .then((res) =>{
-        console.log(res)
-        const data = [];
-        const menuChurros = () => {
-            res.data.map((item) =>{
-              data.push(item)
-              console.log(data);
-            });
-            return data
-        }
-        menuChurros()
-        this.setState({
-            subCategory: data.category
-        })
-    })
-  }
-  addProduct = () => {};
+export default class MenuNav extends React.Component {
+    state = {
+        categories: [],
+        products: []
+    }
 
-  render() {
-    console.log(this.state.selectedItem)
-    const command = this.state;
-    console.log(command);
-    
-    let total = this.state.name + this.state.price;
-    return (
-      <div>
-        <ul className="nav">
-         <button>
-             
-         </button>
-        </ul>
-        </div>
-      
-    );
-  }
+    categories = ["churros","malteadas", "chocolate caliente", "bebidas frias", "helado frito", "paquetes", "extras"]
+
+    componentDidMount() {
+        Axios.get(`https://churro-latte.herokuapp.com/products`)
+            .then(res => {
+                const products = res.data;
+                let categories = [];
+                let category = {};
+                let i;
+
+                for (i = 0; i < this.categories.length; i++){
+                    category.name = this.categories[i]
+                    category.products = products.filter((products)=> {
+                       return products.category===this.categories[i]
+                    })
+                    categories.push(category)
+                } 
+
+                this.setState({ categories });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.categories.map((item, id) =>{
+                    return (
+                        <li className="block" >
+                        <button key={id}
+                          onClick={() => this.setState({categories: item.products})}>
+                          {item.name}
+                        </button>
+                      </li>
+                    )
+
+                })}
+            </div>
+        )
+    }
 }
-
-export default Menu;
-
-
-
-
-
-
-/*import React, { Component } from "react";
-import "./Menu.css";
-import menuChurros from "../Menu";
-import Comanda from "./Comanda";
-import Axios from 'axios';
-
-class Menu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      subCategory: [],
-      selectedItem: [],
-      total: 0
-    };
-  }
-  componentDidMount() {
-    Axios.get('https://churro-latte.herokuapp.com/products')
-    .then((res) =>{
-        console.log(res)
-        const data = res.data.category
-        console.log(data)
-        this.setState({
-             data
-            
-        })
-    })
-  }
-  addProduct = () => {};
-
-  render() {
-    
-    let total = this.state.name + this.state.price;
-    return (
-      <div>
-        <ul className="nav">
-          {menuChurros.categories.map((item, key) => {
-            return (
-              <li className="block" key={key}>
-                <button
-                  key={item.key}
-                  onClick={() =>
-                    this.setState({ subCategory: item.subcategories })
-                  }
-                >
-                  {item.name}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-        <div>
-          {this.state.subCategory
-            ? this.state.subCategory.map((item, index) => {
-                return (
-                  <li className="list" key={item.id}>
-                    <img className="image" alt="" src={item.url}></img>
-                    <h6 className="form" key={item.id}>
-                      {item.name}
-                    </h6>
-                    <p>${item.price}</p>
-
-                    <button
-                      onClick={() =>
-                        this.setState({
-                          selectedItem: [...this.state.selectedItem, item],
-                          total
-                        })
-                      }
-                      className="sendOrder"
-                    >
-                      Ordenar
-                    </button>
-                  </li>
-                );
-              })
-            : null}
-          <div>
-          
-            {console.log(...[this.state.selectedItem])}
-
-          </div>
-        </div>
-        
-      </div>
-    );
-  }
-}
-
-export default Menu;*/
