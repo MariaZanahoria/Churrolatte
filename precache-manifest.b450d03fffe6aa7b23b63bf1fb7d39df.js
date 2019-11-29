62dd502f688928df1d665bcbc045cea0734cf1f5
MariaZanahoria <m.iselacarrillo.p@gmail.com>
2019-11-29 14:53:35 -0600

    Updates

---
+++ precache-manifest.b450d03fffe6aa7b23b63bf1fb7d39df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224554db439b6152ce188d2210f84b14",
+    "url": "/Churrolatte/index.html"
+  },
+  {
+    "revision": "6f494a411287c0a56d84",
+    "url": "/Churrolatte/static/css/main.2a9f8395.chunk.css"
+  },
+  {
+    "revision": "6d6a2b7e2e13793ed485",
+    "url": "/Churrolatte/static/js/2.8e3faf16.chunk.js"
+  },
+  {
+    "revision": "6f494a411287c0a56d84",
+    "url": "/Churrolatte/static/js/main.7f72762e.chunk.js"
+  },
+  {
+    "revision": "81d8e601411a74ad629f",
+    "url": "/Churrolatte/static/js/runtime-main.4a809801.js"
+  }
+]);
